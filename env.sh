c6b00fd3fcf2e833497d6b8326ee3558fdb6ed8d
Cees van de Griend <cees.van.de.griend@protonmail.com>
2023-11-25 14:23:03 +0100

    My first Hello World DAG

--- env.sh
+++ env.sh
@@ -4,4 +4,11 @@ PRJ_DIR=$(pwd)
 source ${PRJ_DIR}/.venv/bin/activate
 
 export AIRFLOW_HOME="${PRJ_DIR}/airflow"
+
 export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://airflow_user:airflow_pass@localhost/airflow_db"
+
+export AIRFLOW__CORE__EXECUTOR="LocalExecutor"
+export AIRFLOW__CORE__LOAD_EXAMPLES="False"
+export AIRFLOW__CORE__DEFAULT_TIMEZONE="Europe/Amsterdam"
+
+export AIRFLOW__WEBSERVER__DEFAULT_UI_TIMEZONE="Europe/Amsterdam"
